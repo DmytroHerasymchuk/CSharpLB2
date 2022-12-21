9a3a623d8c7e12fa56e32c0abdba5f79277e9bb8
Дмитро Герасимчук <dmytro.herasymchuk@nure.ua>
2022-12-21 12:31:21 +0200

    Functional Classes Car and Worker

--- CSharpLB2/Models/Car.cs
+++ CSharpLB2/Models/Car.cs
@@ -9,6 +9,29 @@ namespace CSharpLB2.Models
 {
     public class Car
     {
-
+        public CarBrand Brand { get; set; }
+        public string Name { get; set; }
+        public ControlType ControlType { get; set; }
+        FuelType FuelType { get; set; }
+        public double EngineVolume { get; set; }
+        public double FuelPer100KM { get; set; }
+        public uint PriceForShop { get; set; } 
+        public uint PriceForClient {
+            get
+            {
+                return PriceForClient;
+            }
+            set
+            {
+                if (value > PriceForShop)
+                {
+                    PriceForClient = value;
+                }
+                else
+                {
+                    PriceForClient = 0;
+                }
+            }
+        }
     }
 }
--- CSharpLB2/Models/Worker.cs
+++ CSharpLB2/Models/Worker.cs
@@ -15,7 +15,42 @@ namespace CSharpLB2.Models
         public Education Education { get; set; }
         public Position Position { get; set; }
         public DateTime Birthday { get; set; }
-        public int Salary { get; set; }
-        
+        public int Salary
+        {
+            get
+            {
+                return Salary;
+            }
+            set
+            {
+                if(value >= 8000 && value <= 60000)
+                {
+                    Salary = value; 
+                }
+            }
+        }
+
+        public Worker(string name, string lastName, Education education, Position position, DateTime birthday, int salary)
+        {
+            Name = name;
+            LastName = lastName;
+            IPN = GenerateIPN();
+            Education = education;
+            Position = position;
+            Birthday = birthday;
+            Salary = salary;
+        }
+
+        private string GenerateIPN()
+        {
+            Random random = new Random();
+            string numbers = "0123456789";
+            var buffer = new char[10];
+            for (int i = 0; i < 10; i++)
+            {
+                buffer[i] = numbers[random.Next(numbers.Length)];
+            }
+            return buffer.ToString();
+        }
     }
 }
